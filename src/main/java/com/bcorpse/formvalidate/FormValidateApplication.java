826951fb2b52da7ed0995d7685303517f35923bb
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2018-09-20 13:11:56 +0700

    init project

---
+++ src/main/java/com/bcorpse/formvalidate/FormValidateApplication.java
@@ -0,0 +1,12 @@
+package com.bcorpse.formvalidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormValidateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormValidateApplication.class, args);
+	}
+}
